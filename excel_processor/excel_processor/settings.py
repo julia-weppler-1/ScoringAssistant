133b3758a463777c8cba48f348ca50b777358d68
wepplerj <127237597+JuliaWeppler@users.noreply.github.com>
2024-03-31 21:03:31 -0400

    heroku

--- excel_processor/excel_processor/settings.py
+++ excel_processor/excel_processor/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-%4=h!d)uk%77ni!+r)yukrq7d1^3m!ze%@)c&wrbbo+5&s+22(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://scoring-assistant-f3b2293a68ff.herokuapp.com/']
 
 
 # Application definition
